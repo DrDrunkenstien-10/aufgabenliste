dbfa80f71c592a21340994d04b1115746e3649d8
Atharva Jadhav <drdrunkenstien10@gmail.com>
2025-09-14 13:01:41 +0530

    Add code related to self hosting

--- src/main/java/com/aufgabenliste/config/CorsConfig.java
+++ src/main/java/com/aufgabenliste/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/v1/**")
-                        .allowedOrigins("http://127.0.0.1:5500") // your frontend origin
+                        .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500")
                         .allowedMethods("GET", "POST", "PATCH", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
